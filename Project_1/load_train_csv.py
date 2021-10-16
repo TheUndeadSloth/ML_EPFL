5edaf2a9f5c2e1d4b2e3d7fcfd0eded3eb955cb7
TheUndeadSloth <63819360+TheUndeadSloth@users.noreply.github.com>
2021-10-16 18:04:35 +0200

    Update load_train_csv.py

--- Project_1/load_train_csv.py
+++ Project_1/load_train_csv.py
@@ -1,2 +1,6 @@
 import numpy as np
-traincsv = np.loadtxt('Data/train.csv',delimiter=',',skiprows=1,unpack=True)
+def load_train():
+    traincsv = np.loadtxt('Data/train.csv',delimiter=',',skiprows=1,unpack=True)
+    x = traincsv[2:,:]
+    y = traincsv[1,:]
+    return x,y 
