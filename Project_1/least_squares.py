89898eca759f8e1e17043a3c45dec67790d08666
TheUndeadSloth <63819360+TheUndeadSloth@users.noreply.github.com>
2021-10-21 16:34:49 +0200

    fixed Least squares

--- Project_1/least_squares.py
+++ Project_1/least_squares.py
@@ -1,10 +1,10 @@
 import numpy as np
 def least_squares(y, tx):
-    
+    print("hej")
     """calculate the least squares solution."""
    
-    w = np.linalg.solve((np.transpose(tx) @ tx), np.transpose(tx) @ y)
-    print(w)
+    w = np.linalg.solve((tx.T @ tx), y @ tx)
+    
     e = y-tx @ w
     mse  = 1/(2*len(y))*e@np.transpose(e)
     # ***************************************************
