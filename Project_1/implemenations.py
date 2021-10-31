8022d8435d672a8ca419392731233acc102f7cc5
LobsterChampagne <ferdinand.ytteborg@epfl.ch>
2021-10-31 11:13:39 +0100

    Setup implementations

---
+++ Project_1/implemenations.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+"""
+every function returns (w, loss)
+"""
+least_squares_GD(y, tx, initial_w, max_iters, gamma):
+
+    return w, loss
+
+least_squares_GD(y, tx, initial_w, max_iters, gamma):
+
+    return w, loss
+
+least_squares(y, tx):
+
+    return w, loss
+
+ridge_regression(y, tx, lambda_):
+
+    return w, loss
+
+logistic_regression(y, tx, lambda_):
+
+    return w, loss
+
+reg_logistic_regression(y, tx, initial_w, max_iters, gamma):
+
+    return w, loss
